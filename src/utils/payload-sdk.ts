5f498b438f3e75264f7e6b9bfa5d13f802c5cef7
ArseniyDev <jkl@usa.com>
2025-07-11 21:49:06 +0300

    chore: change storefront port

--- src/utils/payload-sdk.ts
+++ src/utils/payload-sdk.ts
@@ -5,13 +5,13 @@ import { PayloadSDK } from "@shopnex/payload-sdk";
 const isBrowser = typeof window !== "undefined";
 
 export const payloadSdk = new PayloadSDK<Config>({
-    baseURL: `${process.env.NEXT_PUBLIC_SERVER_URL}/api`,
     baseInit: {
+        credentials: "include", // Add this line to include cookies
         headers: {
             "x-shop-handle": process.env.NEXT_PUBLIC_SHOP_HANDLE!,
             "x-shop-id": process.env.NEXT_PUBLIC_SHOP_ID!,
         },
-        credentials: "include", // Add this line to include cookies
     },
+    baseURL: `${process.env.NEXT_PUBLIC_SERVER_URL}/api`,
     fetch: isBrowser ? (...args) => window.fetch(...args) : undefined,
 });
