bacd79b8c4a8cc3923b9e8b8f1687b84d3158d93
ArseniyDev <jkl@usa.com>
2025-07-12 12:01:56 +0300

    chore: fix image url according to be absolute to headless

--- src/utils/map-products.ts
+++ src/utils/map-products.ts
@@ -2,16 +2,22 @@ import type { Product } from "@shopnex/types";
 
 export const mapProducts = (products: Product[]) => {
     return products
-        .map((product) => ({
-            ...product,
-            variants: product.variants?.map((variant) => ({
-                ...variant,
-                imageUrl:
-                    variant.imageUrl ||
-                    (typeof variant.gallery?.[0] === "object"
-                        ? variant.gallery[0].url
-                        : undefined),
-            })),
-        }))
+        .map((product) => {
+            return {
+                ...product,
+                variants: product.variants?.map((variant) => {
+                    const imageUrl =
+                        variant.imageUrl ||
+                        (typeof variant.gallery?.[0] === "object"
+                            ? variant.gallery[0].url
+                            : undefined);
+                    const formattedImageUrl = `${process.env.NEXT_PUBLIC_SERVER_URL}${imageUrl}`;
+                    return {
+                        ...variant,
+                        imageUrl: formattedImageUrl,
+                    };
+                }),
+            };
+        })
         .filter((product) => product.visible);
 };
