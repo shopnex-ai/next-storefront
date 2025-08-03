ce2954d5a58e818cef45865b36302ee70c3e5843
ArseniyDev <jkl@usa.com>
2025-08-03 17:17:58 +0300

    feat: add API rewrites and update @shopnex/types dependency to workspace

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,14 @@ const nextConfig: NextConfig = {
             "localhost",
         ],
     },
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/:path*`,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
